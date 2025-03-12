7aa07d45b4af904a1a3c02811e61eaa279aa178e
marekbacic <marekbacic@gmail.com>
2025-03-12 13:59:43 +0100

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,10 @@
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
-
-  apiKey: "AIzaSyD1u7AlVYAUluw9jPQpEvg8bLX5gMw1jzk",
-
-  authDomain: "haugchemie-d0bdb.firebaseapp.com",
-
-  projectId: "haugchemie-d0bdb",
-
-  storageBucket: "haugchemie-d0bdb.firebasestorage.app",
-
-  messagingSenderId: "537595385",
-
-  appId: "1:537595385:web:a306417977cab05f674128"
-
+apiKey: "AIzaSyD1u7AlVYAUluw9jPQpEvg8bLX5gMw1jzk",
+authDomain: "haugchemie-d0bdb.firebaseapp.com",
+projectId: "haugchemie-d0bdb",
+storageBucket: "haugchemie-d0bdb.firebasestorage.app",
+messagingSenderId: "537595385",
+appId: "1:537595385:web:a306417977cab05f674128"
 };
 
